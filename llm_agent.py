65d095190c6638d44b9c9be93a39c74575f9a344
Kostas Voudouris <strifonline@yahoo.com>
2025-05-19 22:36:39 +0100

    initial commit 233

--- llm_agent.py
+++ llm_agent.py
@@ -1,9 +1,11 @@
+import streamlit as st
 from langchain_community.chat_models import ChatOpenAI
 from langchain.chains import ConversationChain
 from langchain.memory import ConversationBufferMemory
 
 def get_conversational_agent():
-    llm = ChatOpenAI(temperature=0.7)
+    api_key = st.secrets["openai"]["api_key"]
+    llm = ChatOpenAI(openai_api_key=api_key, temperature=0.7)
     memory = ConversationBufferMemory()
     agent = ConversationChain(llm=llm, memory=memory)
     return agent
